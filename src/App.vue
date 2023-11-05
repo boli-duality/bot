7c5be355858946003588d4ab6d754cbf8a262f43
boli <youbolin001@163.com>
2023-11-05 22:39:18 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main />
+</template>
+
+<style scoped>
+</style>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+
+import App from './App.vue'
+import '~/common/styles/main.css'
+
+createApp(App).mount('#app')
